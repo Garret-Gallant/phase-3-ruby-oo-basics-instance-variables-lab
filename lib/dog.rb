c12cc2f63938ce354bb76596da8d72fdeed2cd81
Garret Gallant <gallanttechnological@gmail.com>
2022-08-09 11:31:57 -0600

    commit tree 6f252657fa98bb70cd53fdf115afb54d6841ddeb

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  #setter
+  def set_name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
+  #getter
+  def get_name
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.set_name = "Lassie"
+
+puts lassie.get_name
+
